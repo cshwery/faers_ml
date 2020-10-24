f525f0269a58bcc6915184daf361cb10c435fb7e
cshwery <cshwery@gmail.com>
2020-10-24 16:27:57 -0400

    Initialize from cookiecutter-datascience repo format

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Explore the Faers api and have some fun',
+    author='cshwery@gmail.com',
+    license='MIT',
+)
